3f771e5e93dfdd102dd88b606622e11cdb9c3385
Jakob, Rasmussen <jakob.rasmussen@mullenmail.com>
2018-02-09 14:41:48 -0700

    introductions (minus password)

--- Inventory Controller/src/WriteFile.java
+++ Inventory Controller/src/WriteFile.java
@@ -19,11 +19,7 @@ public class WriteFile
 
             // Note that write() does not automatically
             // append a newline character.
-            bufferedWriter.write("Hello there,");
-            bufferedWriter.write(" here is some text.");
             bufferedWriter.newLine();
-            bufferedWriter.write("We are writing");
-            bufferedWriter.write(" the text to the file.");
 
             // Always close files.
             bufferedWriter.close();
