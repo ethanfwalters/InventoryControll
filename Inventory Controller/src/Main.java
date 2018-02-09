8ba234028c1d2570e1065ded15317f7b1d2fed0e
Jakob, Rasmussen <jakob.rasmussen@mullenmail.com>
2018-02-09 14:39:21 -0700

    introduction (up to password)

--- Inventory Controller/src/Main.java
+++ Inventory Controller/src/Main.java
@@ -2,14 +2,38 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Main
 {
 	public static void main(String [] args) 
 	{
+		String employeePassword = "password";
+		boolean isEmployee = false;
+		
+		Scanner in = new Scanner (System.in);
+		System.out.println("Hello. Are you a costumer or an employee?");
+		String custOrEmp = in.nextLine();
+		
+		if (custOrEmp.contains("ostumer")) System.out.println("Welcome valued customer");
+		if (custOrEmp.contains("mployee"))
+		{
+			System.out.println("Please enter password");
+			String password = in.nextLine();
+			if (password.equals(employeePassword)) 
+			{
+				System.out.println("Welcome back"); isEmployee = true;
+			}
+			else 
+			{
+				System.out.println("That is not the correct password.");
+			}
+		}
+		
 		// The name of the file to open.
         String fileName = "HereItIs.txt";
 
+        
         // This will reference one line at a time
         String line = null;
 
