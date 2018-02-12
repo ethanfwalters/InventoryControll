410b203458310fcd26d5d4e837c8b3d36a66fc88
Chase Sieradski <cmsieradski@gmail.com>
2018-02-12 09:26:27 -0700

    Added txt file to inventory ArrayList

--- Inventory Controller/src/InventoryAdd.java
+++ Inventory Controller/src/InventoryAdd.java
@@ -3,7 +3,7 @@ import java.io.*;
 import java.text.*;
 public class InventoryAdd 
 	{
-	ArrayList <Stuff> inventory = new ArrayList <Stuff>();
+	static ArrayList <Stuff> inventory = new ArrayList <Stuff>();
 	public static void addArray() throws IOException
 		{
 		Scanner file = new Scanner(new File ("HereItIs.txt"));
@@ -11,6 +11,8 @@ public class InventoryAdd
 			{
 			String line = file.nextLine();
 			String[] arr = line.split(" ");
+			arr[1] = arr[1].replaceAll("[^\\p{L}\\p{Nd}]+", " ");
+			inventory.add(new Stuff(Integer.valueOf(arr[0]), arr[1], Integer.valueOf(arr[2]), Integer.valueOf(arr[3])));
 			}
 		}
 	}
