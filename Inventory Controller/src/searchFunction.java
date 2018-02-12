c70213fcda81b8af59a31cc79dbb0db7a42baf62
dimitris.flores@mullenmail.com <Dimitris.Flores@MU-C1-10.mullen.pvt.k12.co.us>
2018-02-12 09:10:53 -0700

    initial commit

---
+++ Inventory Controller/src/searchFunction.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class searchFunction
+	{
+
+		public static void main(String[] args)
+			{
+			Scanner user = new Scanner(System.in);
+			System.out.println("Hello! Anything you would like to search for today?");
+			String person = user.nextLine();
+			String []array = {};
+			
+			while(user.contains(arrayListName))
+				{
+				
+				}
+
+			}
+
+	}
